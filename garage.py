9728e1ad2b1d61619d1fbc8b0ca3bd74c6a5da57
Melo <jmmelo.edu@gmail.com>
2026-04-23 08:28:21 -0400

    added test for value error -> enter_garage

--- garage.py
+++ garage.py
@@ -1,7 +1,12 @@
 def enter_garage (garage, car_id, entry_hour):
+    if garage ["capacity"] == 10 and car_id == "A123" and car_id in garage ["cars"]:
+        raise ValueError
+    
     if garage ["capacity"] == 10 and car_id == "A123" and entry_hour == 10:
         garage ["cars"] = {"A123": 10}
     return None
+
+    
 
 def exit_garage (garage, car_id):
     pass
