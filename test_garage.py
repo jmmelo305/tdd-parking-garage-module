c3546cfcf10b7458a347a8dcc4989daf0d59783c
Melo <jmmelo.edu@gmail.com>
2026-04-23 08:25:41 -0400

    organizing and fixing test names

--- test_garage.py
+++ test_garage.py
@@ -1,5 +1,5 @@
 from garage import get_available_spots, enter_garage
-
+# enter_garage
 def test_enter_garage_A123_at_10():
     garage = {
         "capacity": 10,
@@ -10,22 +10,22 @@ def test_enter_garage_A123_at_10():
     assert garage["cars"] == {"A123": 10}
     assert result is None
 
-
-def test_available_spots_passing():
+# get_available_spots
+def test_get_available_spots_passing():
     garage = {
         "capacity": 10,
         "cars": {}
     }
     assert get_available_spots(garage) == 10
 
-def test_available_spots_spot_taken():
+def test_get_available_spots_spot_taken():
     garage = {
         "capacity": 10,
         "cars": {"A", 1}
     }
     assert get_available_spots(garage) == 8
 
-def test_available_spots_never_negative ():
+def test_get_available_spots_never_negative ():
     garage = {
         "capacity": 1,
         "cars": {"A", 1, "B", 2}
