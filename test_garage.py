c970e69f2fe77a06855dad786614a84dc3f87195
Melo <jmmelo.edu@gmail.com>
2026-04-23 08:09:58 -0400

    Implemented a negative test for available spots

--- test_garage.py
+++ test_garage.py
@@ -14,3 +14,10 @@ def test_available_spots_spot_taken():
         "cars": {"A", 1}
     }
     assert get_available_spots(garage) == 8
+
+def test_available_spots_never_negative ()
+    garage = {
+        "capacity": 1,
+        "cars": {"A", 1, "B", 2}
+    }
+    assert get_available_spots(garage) == 0
