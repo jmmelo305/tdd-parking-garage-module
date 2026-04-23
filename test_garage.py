54f28a8438156acfe8320dfb9770e123a3d300f1
Melo <jmmelo.edu@gmail.com>
2026-04-23 07:58:30 -0400

    Fixed test_enter_garage_passing test

--- test_garage.py
+++ test_garage.py
@@ -1,4 +1,8 @@
 from garage import enter_garage
 
 def test_enter_garage_passing():
-    assert enter_garage(1, 1, 6)
+    garage = {
+        "capacity": 10,
+        "cars": {}
+    }
+    assert enter_garage(garage) == 15
